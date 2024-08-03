2586062fe8a90a7914c3bd0e8a1bb95479a48b94
romanchechyotkin <romanchechyotkin@gmail.com>
2024-08-03 22:40:16 +0300

    flat create route added

--- internal/repo/flat/flat.go
+++ internal/repo/flat/flat.go
@@ -3,8 +3,10 @@ package flat
 import (
 	"context"
 	"log/slog"
+	"time"
 
 	"github.com/romanchechyotkin/avito_test_task/internal/entity"
+	"github.com/romanchechyotkin/avito_test_task/pkg/logger"
 	"github.com/romanchechyotkin/avito_test_task/pkg/postgresql"
 )
 
@@ -21,13 +23,37 @@ func NewRepo(log *slog.Logger, pg *postgresql.Postgres) *Repo {
 }
 
 func (r *Repo) CreateFlat(ctx context.Context, flat *entity.Flat) (*entity.Flat, error) {
+	var err error
+
+	tx, err := r.Pool.Begin(ctx)
+	if err != nil {
+		r.log.Debug("failed to start transaction", logger.Error(err))
+		return nil, err
+	}
+
+	defer func() {
+		if err != nil {
+			r.log.Debug("rollbacking transaction")
+			if err = tx.Rollback(ctx); err != nil {
+				r.log.Error("failed to rollback transaction", logger.Error(err))
+				return
+			}
+		} else {
+			r.log.Debug("committing transaction")
+			if err = tx.Commit(ctx); err != nil {
+				r.log.Error("failed to commit transaction", logger.Error(err))
+				return
+			}
+		}
+	}()
+
 	q := `INSERT INTO flats (number, house_id, price, rooms_amount) VALUES ($1, $2, $3, $4)
 	RETURNING id, number, house_id, price, rooms_amount, moderation_status, created_at
 `
 
 	r.log.Debug("create flat query", slog.String("query", q))
 
-	if err := r.Pool.QueryRow(ctx, q, flat.Number, flat.HouseID, flat.Price, flat.RoomsAmount).Scan(
+	if err = tx.QueryRow(ctx, q, flat.Number, flat.HouseID, flat.Price, flat.RoomsAmount).Scan(
 		&flat.ID,
 		&flat.Number,
 		&flat.HouseID,
@@ -38,6 +64,16 @@ func (r *Repo) CreateFlat(ctx context.Context, flat *entity.Flat) (*entity.Flat,
 	); err != nil {
 		return nil, err
 	}
+
+	q = `UPDATE houses SET updated_at = $1 WHERE id = $2`
+
+	r.log.Debug("update house query", slog.String("query", q))
+	exec, err := tx.Exec(ctx, q, time.Now(), flat.HouseID)
+	if err != nil {
+		return nil, err
+	}
+
+	r.log.Debug("update result", slog.Int64("rows affected", exec.RowsAffected()))
 
 	return flat, nil
 }
