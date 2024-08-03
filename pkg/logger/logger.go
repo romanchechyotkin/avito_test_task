afe3d4f5aa69fe4d607f60904488cb53773782dc
romanchechyotkin <romanchechyotkin@gmail.com>
2024-08-03 11:06:42 +0300

    logger added

---
+++ pkg/logger/logger.go
@@ -0,0 +1,45 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+	"strings"
+)
+
+const defaultLevel = slog.LevelDebug
+
+// todo refactor => devLogger() prodLogger()
+func New() *slog.Logger {
+	var handler slog.Handler
+
+	if env := os.Getenv("APP_ENV"); env == "prod" {
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: configLevel(),
+		})
+	} else {
+		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: configLevel(),
+		})
+	}
+
+	return slog.New(handler)
+}
+
+func configLevel() slog.Level {
+	var logLevel slog.Level
+
+	switch strings.ToLower(os.Getenv("LOG_LEVEL")) {
+	case "debug":
+		logLevel = slog.LevelDebug
+	case "info":
+		logLevel = slog.LevelInfo
+	case "warn":
+		logLevel = slog.LevelWarn
+	case "error":
+		logLevel = slog.LevelError
+	default:
+		logLevel = defaultLevel
+	}
+
+	return logLevel
+}
