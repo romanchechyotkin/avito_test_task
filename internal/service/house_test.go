2dfe3e5c98375323544693579f9e2d24b369f53b
romanchechyotkin <romanchechyotkin@gmail.com>
2024-08-06 15:34:25 +0300

    get house flats integration tests added

--- internal/service/house_test.go
+++ internal/service/house_test.go
@@ -4,10 +4,11 @@ package service
 
 import (
 	"context"
-	"github.com/romanchechyotkin/avito_test_task/internal/entity"
+	"fmt"
 	"log/slog"
 	"testing"
 
+	"github.com/romanchechyotkin/avito_test_task/internal/entity"
 	"github.com/romanchechyotkin/avito_test_task/internal/repo"
 	"github.com/romanchechyotkin/avito_test_task/pkg/utest"
 
@@ -66,6 +67,63 @@ func TestHouseService_CreateHouse(t *testing.T) {
 	})
 }
 
-func TestHouseService_GetHouseFlats(t *testing.T) {}
+func TestHouseService_GetHouseFlats(t *testing.T) {
+	require.NoError(t, prepareErr)
 
-func TestHouseService_GetHouse(t *testing.T) {}
+	log.Debug("test configuration", slog.Any("cfg", cfg.Postgresql))
+
+	defer utest.TeardownTable(log, pg, "houses")
+
+	repositories := repo.NewRepositories(log, pg)
+
+	flatService := NewFlatService(log, NewSenderService(log, repositories.House), repositories.Flat)
+	houseService := NewHouseService(log, repositories.House, repositories.Flat)
+
+	var houseID uint
+
+	t.Run("successful getting flat for house", func(t *testing.T) {
+		log.Debug("creating house")
+		house, err := houseService.CreateHouse(context.Background(), &HouseCreateInput{
+			Address: "Улица Пушкина 1",
+			Year:    1999,
+		})
+		require.NoError(t, err)
+		require.Equal(t, "", house.Developer.String)
+		houseID = house.ID
+
+		log.Debug("creating flat")
+		flat, err := flatService.CreateFlat(context.Background(), &FlatCreateInput{
+			Number:      1,
+			HouseID:     house.ID,
+			Price:       1,
+			RoomsAmount: 1,
+		})
+		require.NoError(t, err)
+		require.Equal(t, house.ID, flat.HouseID)
+		require.Equal(t, "created", flat.ModerationStatus)
+
+		houseFlats, err := houseService.GetHouseFlats(context.Background(), &GetHouseFlatsInput{
+			HouseID:  fmt.Sprintf("%d", house.ID),
+			UserType: "client",
+		})
+		require.NoError(t, err)
+		require.Equal(t, 0, len(houseFlats))
+
+		houseFlats, err = houseService.GetHouseFlats(context.Background(), &GetHouseFlatsInput{
+			HouseID:  fmt.Sprintf("%d", house.ID),
+			UserType: "moderator",
+		})
+		require.NoError(t, err)
+		require.Equal(t, 1, len(houseFlats))
+	})
+
+	t.Run("failed getting flat for non existing house", func(t *testing.T) {
+		houseFlats, err := houseService.GetHouseFlats(context.Background(), &GetHouseFlatsInput{
+			HouseID:  fmt.Sprintf("%d", houseID),
+			UserType: "client",
+		})
+		require.ErrorIs(t, err, ErrHouseNotFound)
+		require.Equal(t, ([]*entity.Flat)(nil), houseFlats)
+	})
+
+}
