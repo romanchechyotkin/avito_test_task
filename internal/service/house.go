d6733de90cec61ae4191985238611d89baf1de65
romanchechyotkin <romanchechyotkin@gmail.com>
2024-08-06 12:42:46 +0300

    flat create integration tests added
    get house flats integration tests added
    
    get house flats integration tests added
    
    get house flats integration tests added
    
    get house flats integration tests added
    
    get house flats integration tests added

--- internal/service/house.go
+++ internal/service/house.go
@@ -55,11 +55,11 @@ func (s *HouseService) CreateHouse(ctx context.Context, input *HouseCreateInput)
 func (s *HouseService) GetHouseFlats(ctx context.Context, input *GetHouseFlatsInput) ([]*entity.Flat, error) {
 	flats, err := s.flatRepo.GetHouseFlats(ctx, input.HouseID, input.UserType)
 	if err != nil {
-		if errors.Is(err, repoerrors.ErrNotFound) {
-			return nil, ErrHouseNotFound
-		}
-
 		return nil, err
+	}
+
+	if len(flats) == 0 {
+		return nil, ErrHouseFlatsNotFound
 	}
 
 	return flats, nil
