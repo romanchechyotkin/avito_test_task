490ba95f188bf59baf21e575fcd60daf3d6aa093
romanchechyotkin <romanchechyotkin@gmail.com>
2024-08-06 10:32:38 +0300

    auth integration tests added
    auth integration tests added
    
    auth integration tests added
    
    auth integration tests added
    
    auth integration tests added
    
    auth integration tests added
    
    auth integration tests added
    
    auth integration tests added
    
    auth integration tests added
    
    auth integration tests added
    
    auth integration tests added
    
    auth integration tests finished

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"flag"
 	"log/slog"
 	"os"
 	"time"
@@ -10,6 +9,8 @@ import (
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
+
+const defaultConfigFile = "config.yaml"
 
 type Config struct {
 	HTTP       `yaml:"http"`
@@ -39,10 +40,6 @@ type JWT struct {
 
 func New(log *slog.Logger) (*Config, error) {
 	path := fetchConfigPath()
-	if path == "" {
-		log.Error("config path is empty")
-		os.Exit(1)
-	}
 
 	if _, err := os.Stat(path); err != nil {
 		log.Error("failed to open config file", logger.Error(err))
@@ -60,13 +57,10 @@ func New(log *slog.Logger) (*Config, error) {
 }
 
 func fetchConfigPath() string {
-
 	var path string
-	flag.StringVar(&path, "config", "", "sets path to config file")
-	flag.Parse()
 
-	if path == "" {
-		path = os.Getenv("CONFIG_PATH")
+	if path = os.Getenv("CONFIG_PATH"); path == "" {
+		path = defaultConfigFile
 	}
 
 	return path
---
+++ pkg/utest/utest.go
@@ -0,0 +1,51 @@
+package utest
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/romanchechyotkin/avito_test_task/internal/config"
+	"github.com/romanchechyotkin/avito_test_task/pkg/migrations"
+	"github.com/romanchechyotkin/avito_test_task/schema"
+
+	"github.com/romanchechyotkin/avito_test_task/pkg/logger"
+	"github.com/romanchechyotkin/avito_test_task/pkg/postgresql"
+)
+
+func Prepare() (*slog.Logger, *config.Config, *postgresql.Postgres, error) {
+	log := logger.New()
+	cfg, err := config.New(log)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	pg, err := postgresql.New(log, &cfg.Postgresql)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	err = migrations.Migrate(log, &schema.DB, fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		cfg.Postgresql.User,
+		cfg.Postgresql.Password,
+		cfg.Postgresql.Host,
+		cfg.Postgresql.Port,
+		cfg.Postgresql.Database,
+		cfg.Postgresql.SSLMode,
+	))
+	if err != nil {
+		return nil, nil, nil, err
+
+	}
+
+	return log, cfg, pg, nil
+}
+
+func TeardownTable(log *slog.Logger, pg *postgresql.Postgres, tableName string) {
+	exec, err := pg.Pool.Exec(context.Background(), fmt.Sprintf("TRUNCATE TABLE %s CASCADE", tableName))
+	if err != nil {
+		log.Error("failed to truncate users table", logger.Error(err))
+		return
+	}
+	log.Debug("truncated users table", slog.Int64("rows affected", exec.RowsAffected()))
+}
